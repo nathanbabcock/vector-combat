b59d68fca5c1fba1ed2fb051cb83212b64722353
Nathan Babcock <nathan.r.babcock@gmail.com>
2015-09-06 15:27:40 -0700

    Ninja sprites

--- src/model/Game.java
+++ src/model/Game.java
@@ -31,7 +31,7 @@ public class Game {
         setupSprites();
 
         // Spawn player
-        player = new Scout(this);
+        player = new Ninja(this);
         player.position = new Point2D(400, 549);
         player.acceleration.y = gravity;
 
@@ -56,6 +56,9 @@ public class Game {
         sprites.put("rocket_standing", new Sprite(spriteSheet, 0, 0, 24, 80));
         sprites.put("rocket_walking", new Sprite(spriteSheet, 32, 0, 24, 80));
         sprites.put("rocket_launcher", new Sprite(spriteSheet, 64, 0, 64, 24));
+
+        sprites.put("ninja_standing", new Sprite(spriteSheet, 0, 112, 40, 96, 0, 16));
+        sprites.put("ninja_walking", new Sprite(spriteSheet, 48, 112, 40, 96, 0, 16));
 
         sprites.put("soldier_standing", new Sprite(spriteSheet, 0, 216, 32, 80, -8, 0));
         sprites.put("soldier_walking", new Sprite(spriteSheet, 40, 216, 32, 80, -8, 0));
---
+++ src/model/Ninja.java
@@ -0,0 +1,58 @@
+package model;
+
+import view.Canvas;
+
+import java.awt.*;
+
+/**
+ * Created by Nathan on 8/31/2015.
+ */
+public class Ninja extends Player {
+    public Ninja(Game game) {
+        super(game);
+        attackInterval = 1.0f;
+    }
+
+    @Override
+    public void updateSprite(float deltaTime) {
+        if (walkingLeft || walkingRight) {
+            float spriteInterval = 0.25f;
+            if (spriteTime >= spriteInterval) {
+                if (sprite == game.sprites.get("ninja_standing")) {
+                    sprite = game.sprites.get("ninja_walking");
+                } else if (sprite == game.sprites.get("ninja_walking"))
+                    sprite = game.sprites.get("ninja_standing");
+                spriteTime = 0;
+            }
+        } else {
+            sprite = game.sprites.get("ninja_standing");
+        }
+        spriteTime += deltaTime;
+    }
+
+    @Override
+    public void attack(float deltaTime) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas, Graphics2D g2) {
+        // Draw hitbox
+//            g2.setColor(randColor);
+//            g2.fillRect((int) player.getBottomLeft().x + cameraOffsetX, (int) (HEIGHT - cameraOffsetY - player.getBottomLeft().y - player.height), (int) player.width, (int) player.height);
+
+        // Player
+        int playerX = (int) getBottomLeft().x + canvas.cameraOffsetX + sprite.offsetX;
+        int playerY = (int) (canvas.HEIGHT - canvas.cameraOffsetY - getBottomLeft().y - height - sprite.offsetY);
+        int playerWidth = sprite.width;
+        int playerHeight = sprite.height;
+
+        if (xhair.x < getCenter().x) {
+            playerWidth *= -1;
+            playerX += sprite.width - 16;
+        }
+
+        g2.drawImage(sprite.image, playerX, playerY, playerWidth, playerHeight, null);
+        g2.setTransform(canvas.backup);
+    }
+}
